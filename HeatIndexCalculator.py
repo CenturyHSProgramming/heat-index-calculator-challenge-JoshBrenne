70ba7055392569e116822ce9bc9bc411757c20d5
Josh Brenne <joshbrenne@gmail.com>
2017-04-21 23:19:57 -0700

    adding as successful as possible code; results are consistently one off from the desired; the provided website does not have an equation, so I had to research online

--- HeatIndexCalculator.py
+++ HeatIndexCalculator.py
@@ -6,9 +6,20 @@
 
 # Define Function below
 # be sure to return an integer
-
+def calculateHeatIndex(RH, T):
+    a = -42.379
+    b = 2.04901523
+    c = 10.14333127
+    d = -.22475541
+    e = -6.83783*(10**-3)
+    f = -5.481717*(10**-2)
+    g = 1.22874*(10**-3)
+    h = 8.5282*(10**-4)
+    i = -1.99*(10**-6)
+    heatindex = a + b*T + c*RH + d*T*RH + e*T**2 + f*RH**2 + g*RH*T**2 + h*T*RH**2 + i*T**2*RH**2
+    heatindex = int(heatindex)
+    return heatindex
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
     # Make sure it's indented
-    pass # remove or comment out this line if you wish to test the function
